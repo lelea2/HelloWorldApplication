d954b49aa69155f3a5c17dd6b1ae7cdb8135b86b
Khanh Dao <kdao@Khanhs-MBP.attlocal.net>
2016-02-18 03:17:51 -0800

    Initial check in for barcode

--- app/src/main/java/com/example/kdao/helloworldapplication/BarCodeActivity.java
+++ app/src/main/java/com/example/kdao/helloworldapplication/BarCodeActivity.java
@@ -1,18 +1,33 @@
 package com.example.kdao.helloworldapplication;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
-public class BarCodeActivity extends AppCompatActivity {
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+public class BarCodeActivity extends AppCompatActivity implements OnClickListener{
+
+    private Button scanBtn;
+    private TextView formatTxt;
+    private TextView contentTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bar_code);
+        scanBtn = (Button)findViewById(R.id.scan_button);
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        contentTxt = (TextView)findViewById(R.id.scan_content);
+        scanBtn.setOnClickListener(this);
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
@@ -26,4 +41,24 @@ public class BarCodeActivity extends AppCompatActivity {
         });*/
     }
 
+    public void onClick(View v) {
+        if(v.getId()==R.id.scan_button) {
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            scanIntegrator.initiateScan();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanningResult != null) {
+            String scanContent = scanningResult.getContents();
+            String scanFormat = scanningResult.getFormatName();
+            formatTxt.setText("FORMAT: " + scanFormat);
+            contentTxt.setText("CONTENT: " + scanContent);
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
 }
